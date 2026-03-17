c279773c9987663bdddac7f7ee219cddbe045c04
ega394 <ega394@users.noreply.github.com>
2026-03-17 13:47:10 +0800

    Update App.tsx
    hapus capco

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,10 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
-import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
 const App = () => {
-
-  useEffect(() => {
-    const loadUpdater = async () => {
-      if (typeof window !== "undefined" && (window as any).Capacitor) {
-        try {
-          const module = await import('@capgo/capacitor-updater');
-          module.CapacitorUpdater.notifyAppReady();
-          console.log("CapacitorUpdater loaded for mobile.");
-        } catch (err) {
-          console.warn("Failed to load CapacitorUpdater:", err);
-        }
-      }
-    };
-    loadUpdater();
-  }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
